60e90cb2f9c6454505f8ab88cc21edf2a823baf3
Sergio Sanchez <sergio.sanchezs@ingenieros.com>
2020-01-08 12:52:38 -0500

    Create Week1.java

---
+++ Week1.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package week1;
+
+/**
+ * @author 767520
+ *
+ */
+public class Week1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// 1. Print my name
+		// 2. Declared values
+		
+		String name = "Sergio Sanchez";
+		int age = 33;
+		
+		// Print my name and age
+		System.out.println("My name is " + name + " and my age is " + age);
+
+	}
+
+}
