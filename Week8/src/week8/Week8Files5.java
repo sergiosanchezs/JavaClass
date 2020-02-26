d2b48f187d870ca62f466ed44057f81d8eb2fc06
Sergio Sanchez <sergio.sanchezs@ingenieros.com>
2020-02-26 10:38:47 -0500

    Reading a file with Scanner and File classes

--- Week8/src/week8/Week8Files5.java
+++ Week8/src/week8/Week8Files5.java
@@ -2,6 +2,10 @@
  * 
  */
 package week8;
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * @author Sergio Sanchez
@@ -12,8 +16,28 @@ public class Week8Files5 {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException
+	{
+		String str;
+		
+		// Reading a file
+		File filename = new File("names.txt");
+		Scanner inputFile = new Scanner(filename);
+		
+		if (inputFile.hasNextLine()) 
+			System.out.println("The content of the file is:");
+		else
+			System.out.println("No lines to be read");
+		
+		while(inputFile.hasNextLine()) 
+		{
+			str = inputFile.nextLine();
+			System.out.println(str);
+			
+		}
+		
+		inputFile.close();
+
 
 	}
 
