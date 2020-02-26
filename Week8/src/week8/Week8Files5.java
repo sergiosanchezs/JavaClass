037e4d7713abd12075f6d250d828ace58b78e7bc
Sergio Sanchez <sergio.sanchezs@ingenieros.com>
2020-02-26 11:17:01 -0500

    documenting the code

--- Week8/src/week8/Week8Files5.java
+++ Week8/src/week8/Week8Files5.java
@@ -21,22 +21,24 @@ public class Week8Files5 {
 		
 		// Reading a file
 		File fileName = new File("names.txt");
+		
+		// Checking if the file exist
 		if (fileName.isFile()) {
 			
-			Scanner inputFile = new Scanner(fileName);
+			Scanner inputFile = new Scanner(fileName);	// Reading the file with Scanner class
 			
-			if (inputFile.hasNextLine()) 
+			if (inputFile.hasNextLine()) // If the file has content it will read the content
 				System.out.println("The content of the file is:");
-			else
+			else	// if the file doesn't has content show a message to the user
 				System.out.println("No lines to be read");
 			
-			while(inputFile.hasNextLine()) 
+			while(inputFile.hasNextLine()) // while the file has lines to read it will keep reading and printing
 			{
 				str = inputFile.nextLine();
 				System.out.println(str);
 			}
 			
-			inputFile.close();
+			inputFile.close();	// closing the file
 			
 			// deleting a file
 			fileName.delete();
