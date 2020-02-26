203bb59ac693868b8ddd20bd57e0634364c14096
Sergio Sanchez <sergio.sanchezs@ingenieros.com>
2020-02-26 09:47:28 -0500

    first Practice with Java Files
    import java.io.
        PrintWriter
        IOException

---
+++ PracticeTest1/src/practice1/Files2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package practice1;
+
+/**
+ * @author sergi
+ *
+ */
+public class Files2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
