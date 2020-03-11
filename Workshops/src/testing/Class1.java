91e4eacb2774f3ad6ee542382ac5184f46befccb
Sergio Sanchez <sergio.sanchezs@ingenieros.com>
2020-03-11 12:24:26 -0400

    practicing simple classes and objects exercises

---
+++ Workshops/src/testing/Class1.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package testing;
+
+/**
+ * @author sergi
+ *
+ */
+public class Class1 {
+	char c;
+
+}
---
+++ Workshops/src/testing/Class2.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package testing;
+
+/**
+ * @author sergi
+ *
+ */
+public class Class2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Class1 obj = new Class1();
+		
+		System.out.println((int)obj.c);
+
+	}
+
+}
